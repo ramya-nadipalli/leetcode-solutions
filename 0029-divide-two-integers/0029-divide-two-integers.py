b5f7c0a05bc09580edcca9a50acdd6d894b1460d
Sri Ramya Nadipalli <sriramyanadipalli@gmail.com>
2026-07-21 12:05:09 +0530

    Time: 0 ms (100%), Space: 19.2 MB (65.95%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.py
@@ -0,0 +1,30 @@
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        INT_MAX = 2**31 - 1
+        INT_MIN = -2**31
+
+        if dividend == INT_MIN and divisor == -1:
+            return INT_MAX
+
+        negative = (dividend < 0) != (divisor < 0)
+
+        dividend = abs(dividend)
+        divisor = abs(divisor)
+
+        ans = 0
+
+        while dividend >= divisor:
+            temp = divisor
+            multiple = 1
+
+            while dividend >= (temp << 1):
+                temp <<= 1
+                multiple <<= 1
+
+            dividend -= temp
+            ans += multiple
+
+        if negative:
+            ans = -ans
+
+        return ans
