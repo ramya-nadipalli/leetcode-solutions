ad237db4a61acea2e6fe899b4b506d9338eb8b38
Sri Ramya Nadipalli <sriramyanadipalli@gmail.com>
2026-08-15 22:16:09 +0530

    Time: 0 ms (100%), Space: 19 MB (99.64%) - LeetHub

---
+++ 0086-partition-list/0086-partition-list.py
@@ -0,0 +1,22 @@
+class Solution:
+    def partition(self, head, x):
+        before = ListNode(0)
+        after = ListNode(0)
+
+        b = before
+        a = after
+
+        while head:
+            if head.val < x:
+                b.next = head
+                b = b.next
+            else:
+                a.next = head
+                a = a.next
+
+            head = head.next
+
+        a.next = None
+        b.next = after.next
+
+        return before.next
